7c354a3b8c5136d252454de5d1879d9bad1cf160
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2018-08-15 19:13:53 +0800

    build fastai on top of scipy-notebook

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,4 @@ docker build -t leoyuholo/nvidia-docker nvidia-docker
 docker build -t leoyuholo/base-notebook-gpu base-notebook
 docker build -t leoyuholo/minimal-notebook-gpu minimal-notebook
 docker build -t leoyuholo/scipy-notebook-gpu scipy-notebook
-docker build -t leoyuholo/datascience-notebook-gpu datascience-notebook
-docker build -t leoyuholo/tensorflow-notebook-gpu tensorflow-notebook
-docker build -t leoyuholo/pytorch-notebook-gpu pytorch-notebook
 docker build -t leoyuholo/fastai-notebook-gpu fastai-notebook
