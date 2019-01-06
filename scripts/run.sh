4d21df502f91cc75aed49c1b3f4b727d436b5606
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2019-01-06 14:42:55 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,11 @@
-nvidia-docker run \
-              --rm \
-              --name jupyter \
-              -p 8088:8888 \
-              -e NB_UID=$(id -u) \
-              -e NB_GID=$(id -g) \
-              -u 0 \
-              -v /home/leo/ws:/home/jovyan \
-              leoyuholo/fastai-notebook-gpu \
-              start.sh jupyter notebook
+docker run \
+       --runtime=nvidia \
+       --rm \
+       --name jupyter \
+       -p 8088:8888 \
+       -e NB_UID=$(id -u) \
+       -e NB_GID=$(id -g) \
+       -u 0 \
+       -v /home/leo/ws:/home/jovyan \
+       leoyuholo/fastai-notebook-gpu \
+       start.sh jupyter notebook
